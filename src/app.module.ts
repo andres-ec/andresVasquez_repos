cd167826e6cad6867d00002c5779c9d50a11789a
avasquez <avasquez@montran.com>
2023-05-02 13:31:20 -0500

    avasquez: db info encryption

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RepositoriesModule } from './repositories/repositories.module';
 import { OrganizationModule } from './organization/organization.module';
@@ -9,19 +9,22 @@ import { OrganizationModule } from './organization/organization.module';
     ConfigModule.forRoot(),
     RepositoriesModule,
     OrganizationModule,
-    //TODO: Encryption
-    TypeOrmModule.forRoot({
-      type: 'cockroachdb',
-      host: 'sky-catfish-3738.g8z.cockroachlabs.cloud',
-      port: 26257,
-      database: 'deuna',
-      username: 'avasquez',
-      password: 'OpCU-t2EfTqGrGzFAdROYg',
-      ssl: {
-        rejectUnauthorized: false, // Set to true if server has a valid SSL certificate
-      },
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'cockroachdb',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        database: configService.get<string>('DB_NAME'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        ssl: {
+          rejectUnauthorized: false, // Set to true if server has a valid SSL certificate
+        },
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [],
